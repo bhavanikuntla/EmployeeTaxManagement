581c734704d0aab652d99eaf7565c38434e3fd9e
bhavanukuntla <myselfbhavani@yahoo.co.in>
2024-03-22 06:46:16 +0530

    Project Setup

---
+++ src/main/java/com/employee/tax/management/EmployeeTaxManagementApplication.java
@@ -0,0 +1,13 @@
+package com.employee.tax.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeTaxManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeTaxManagementApplication.class, args);
+	}
+
+}
